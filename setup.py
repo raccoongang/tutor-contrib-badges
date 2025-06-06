08156805e1e7c0d57d5b2825315fad8b046da497
Max Sokolski <cmltawt0@gmail.com>
2025-06-06 15:55:57 +0300

    v20.0.0

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.9",
-    install_requires=["tutor>=19.0.0,<20.0.0"],
+    install_requires=["tutor>=20.0.0,<21.0.0"],
     extras_require={
         "dev": [
-            "tutor[dev]>=19.0.0,<20.0.0",
+            "tutor[dev]>=20.0.0,<21.0.0",
         ]
     },
     entry_points={
